50afa57aa255e204250a614e6d6a9a4efc07d8fb
mdhaaib200520 <mdhasib200520@gmail.com>
2026-05-25 08:23:27 +0600

    Update twrp_lake.mk

--- twrp_lake.mk
+++ twrp_lake.mk
@@ -1,29 +1,34 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#
+# Copyright 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Only the below variable(s) need to be changed!
+#
 
-# Inherit from generic device
-$(call inherit-product, device/xiaomi/lake/device.mk)
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/pond/device.mk)
 
-# Inherit some common twrp stuff.
+# Inherit any OrangeFox-specific settings
+#$(call inherit-product-if-exists, device/xiaomi/pond/fox_pond.mk)
+
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Device identifier
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lake
 PRODUCT_NAME := twrp_lake
-PRODUCT_BRAND := Xiaomi
+PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi 14C
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# System properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
-# Release name
-PRODUCT_RELEASE_NAME := lake
-
-# Screen resolution
-TARGET_SCREEN_HEIGHT := 1640
-TARGET_SCREEN_WIDTH := 720
